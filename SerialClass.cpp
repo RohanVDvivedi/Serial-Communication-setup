42f9adade7d4a0dc1baadee53cf960f67f244da1
Rohan Dvivedi <rohandvivedi@gmail.com>
2016-08-28 22:56:43 +0530

    Add files via upload

---
+++ SerialClass.cpp
@@ -0,0 +1,195 @@
+#include "SerialClass.h"
+
+void Serial::Serialinit(char *portName,int baud)
+{
+    //We're not yet connected
+    this->connected = false;
+
+    //Try to connect to the given port throuh CreateFile
+    this->hSerial = CreateFile(portName,
+            GENERIC_READ | GENERIC_WRITE,
+            0,
+            NULL,
+            OPEN_EXISTING,
+            FILE_ATTRIBUTE_NORMAL,
+            NULL);
+
+    //Check if the connection was successfull
+    if(this->hSerial==INVALID_HANDLE_VALUE)
+    {
+        //If not success full display an Error
+        if(GetLastError()==ERROR_FILE_NOT_FOUND){
+
+            //Print Error if neccessary
+            printf("ERROR: Handle was not attached. Reason: %s not available.\n", portName);
+
+        }
+        else
+        {
+            printf("ERROR!!!");
+        }
+    }
+    else
+    {
+        //If connected we try to set the comm parameters
+        DCB dcbSerialParams = {0};
+
+        //Try to get the current
+        if (!GetCommState(this->hSerial, &dcbSerialParams))
+        {
+            //If impossible, show an error
+            printf("failed to get current serial parameters!");
+        }
+        else
+        {
+            //Define serial connection parameters for the arduino board
+            dcbSerialParams.BaudRate=baud;//CBR_9600;
+            dcbSerialParams.ByteSize=8;
+            dcbSerialParams.StopBits=ONESTOPBIT;
+            dcbSerialParams.Parity=NOPARITY;
+            //Setting the DTR to Control_Enable ensures that the Arduino is properly
+            //reset upon establishing a connection
+            dcbSerialParams.fDtrControl = DTR_CONTROL_ENABLE;
+
+             //Set the parameters and check for their proper application
+             if(!SetCommState(hSerial, &dcbSerialParams))
+             {
+                printf("ALERT: Could not set Serial Port parameters");
+             }
+             else
+             {
+                 //If everything went fine we're connected
+                 this->connected = true;
+                 //Flush any remaining characters in the buffers
+                 PurgeComm(this->hSerial, PURGE_RXCLEAR | PURGE_TXCLEAR);
+                 //We wait 2s as the arduino board will be reseting
+                 Sleep(ARDUINO_WAIT_TIME);
+                 PurgeComm(this->hSerial, PURGE_RXCLEAR | PURGE_TXCLEAR);
+                 //We wait 2s as the arduino board will be reseting
+                 Sleep(ARDUINO_WAIT_TIME);
+             }
+        }
+    }
+
+}
+
+Serial::~Serial()
+{
+    //Check if we are connected before trying to disconnect
+    if(this->connected)
+    {
+        //We're no longer connected
+        this->connected = false;
+        //Close the serial handler
+        CloseHandle(this->hSerial);
+    }
+}
+
+int Serial::ReadData(char *buffer, unsigned int nbChar)
+{
+    //Number of bytes we'll have read
+    DWORD bytesRead;
+    //Number of bytes we'll really ask to read
+    unsigned int toRead;
+
+    //Use the ClearCommError function to get status info on the Serial port
+    ClearCommError(this->hSerial, &this->errors, &this->status);
+
+    //Check if there is something to read
+    if(this->status.cbInQue>0)
+    {
+        //If there is we check if there is enough data to read the required number
+        //of characters, if not we'll read only the available characters to prevent
+        //locking of the application.
+        if(this->status.cbInQue>nbChar)
+        {
+            toRead = nbChar;
+        }
+        else
+        {
+            toRead = this->status.cbInQue;
+        }
+
+        //Try to read the require number of chars, and return the number of read bytes on success
+        if(ReadFile(this->hSerial, buffer, toRead, &bytesRead, NULL) && bytesRead != 0)
+        {
+            return bytesRead;
+        }
+
+    }
+
+    //If nothing has been read, or that an error was detected return -1
+    return -1;
+
+}
+
+
+bool Serial::WriteData(char *buffer, unsigned int nbChar)
+{
+    DWORD bytesSend;
+
+    //Try to write the buffer on the Serial port
+    if(!WriteFile(this->hSerial, (void *)buffer, nbChar, &bytesSend, 0))
+    {
+        //In case it don't work get comm error and return false
+        ClearCommError(this->hSerial, &this->errors, &this->status);
+
+        return false;
+    }
+    else
+        return true;
+}
+
+bool Serial::IsConnected()
+{
+    //Simply return the connection status
+    return this->connected;
+}
+
+
+
+// self made functions below
+
+unsigned int Serial::read_uint_serial() // returns serially read 4 byte unsigned integer
+{
+    unsigned int k=0;
+    char c[4];
+    unsigned char uc[4];
+
+    Sleep(2);
+    if(ReadData(c,(unsigned int)4)!=4)
+    {
+        printf("improper\n");
+        return -1;
+    }
+
+    for(k=0;k<4;k++)
+    {
+        uc[k]=(unsigned char)c[k];
+    }
+    //cout<<endl<<(unsigned int)uc[0]<<" "<<(unsigned int)uc[1]<<" "<<(unsigned int)uc[2]<<" "<<(unsigned int)uc[3]<<endl;
+    k=(((unsigned int)uc[0])<<24)+(((unsigned int)uc[1])<<16)+(((unsigned int)uc[2])<<8)+((unsigned int)uc[3]);
+
+    return k;
+}
+
+
+
+void Serial::write_uint_serial(int i) // sends unsigned 4 byte integer serially
+{
+        unsigned char uc[4];
+        uc[3]=i;
+        i=i>>8;
+        uc[2]=i;
+        i=i>>8;
+        uc[1]=i;
+        i=i>>8;
+        uc[0]=i;
+        char c[4];
+        for(i=0;i<4;i++)
+        {
+            c[i]=uc[i];
+        }
+        WriteData(c,4);
+        //cout<<(int)c[0]<<" "<<(int)c[1]<<" "<<(int)c[2]<<" "<<(int)c[3]<<endl;
+}
---
+++ SerialClass.h
@@ -0,0 +1,47 @@
+#ifndef SERIALCLASS_H_INCLUDED
+#define SERIALCLASS_H_INCLUDED
+
+#define ARDUINO_WAIT_TIME 3000
+//#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+
+using namespace std;
+
+class Serial
+{
+    private:
+        //Serial comm handler
+        HANDLE hSerial;
+        //Connection status
+        bool connected;
+        //Get various information about the connection
+        COMSTAT status;
+        //Keep track of last error
+        DWORD errors;
+
+    public:
+        //Initialize Serial communication with the given COM port
+        void Serialinit(char *portName,int baud);
+        //Close the connection
+        ~Serial();
+        //Read data in a buffer, if nbChar is greater than the
+        //maximum number of bytes available, it will return only the
+        //bytes available. The function return -1 when nothing could
+        //be read, the number of bytes actually read.
+        int ReadData(char *buffer, unsigned int nbChar);
+        //Writes data from a buffer through the Serial connection
+        //return true on success.
+        bool WriteData(char *buffer, unsigned int nbChar);
+        //Check if we are actually connected
+        bool IsConnected();
+
+        unsigned int read_uint_serial(); // reads an unsigned number from serial uc
+        void write_uint_serial(int i); //writes an unsigned number to serial uc
+
+};
+
+#endif // SERIALCLASS_H_INCLUDED
